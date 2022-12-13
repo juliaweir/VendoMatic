2ce393a9ccb60d9fbc26d666f6c4528cac4f07bf
Alfred Good <AlfredGoodPersonal@hotmail.com>
2022-12-12 20:38:46 -0700

    Started working on item class and child classes

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -20,6 +20,7 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+				System.out.println("nonononononono");
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
 			}
