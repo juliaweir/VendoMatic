04fb8343d924b8fea696c2cd4a40614a2ec03a9d
Julia <julia.leigh98@gmail.com>
2022-12-19 11:33:51 -0500

    Commit 12/19 Logger class

--- src/main/java/com/techelevator/Logger.java
+++ src/main/java/com/techelevator/Logger.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
